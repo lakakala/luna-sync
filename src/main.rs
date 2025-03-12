882ea3625fb99bdfd5ea1d3059fe621a7f6e51e7
kexin.cola <kexin.cola@outlook.com>
2025-03-12 09:59:07 +0800

    feat: fix

--- src/main.rs
+++ src/main.rs
@@ -55,21 +55,38 @@ impl Executor for GitExecutor {
 
 struct ExecutorContext {
     http_expoter: HttpExporter,
-}   
+}
 
 struct HttpExporter {
     namespaces: HashSet<String>,
 }
 
 impl HttpExporter {
-    fn export_http(&self, namespace :String) -> result::Result<()> {
-        if self.namespaces.contains(&namespace) {
-            
-        }
+    fn export_http(&self, namespace: String) -> result::Result<()> {
+        if self.namespaces.contains(&namespace) {}
         todo!()
     }
 }
 
-struct TcpExport {
+struct TcpExport {}
 
+struct HttpExportServer {}
+
+impl HttpExportServer {
+    async fn start(&self) {
+        use actix_web::{App, HttpServer, web};
+
+        HttpServer::new(|| App::new().route("/git/*", web::to(Self::handle_request)))
+            .bind(("127.0.0.1", 8080))
+            .expect("")
+            .run()
+            .await
+            .expect("")
+    }
+
+    async fn handle_request(
+        req: actix_web::HttpRequest,
+    ) -> std::result::Result<actix_files::NamedFile, actix_web::Error> {
+        todo!()
+    }
 }
